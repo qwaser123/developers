35ed05017cbc0b7f5e6b88be67ce822b006033bc
ksang <1120ksh98@gmail.com>
2023-09-22 22:23:21 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { db } from "./index.js";
+import Footer from "./Footer.js";
+import "firebase/firestore";
+import { useState } from "react";
+import styled from "styled-components";
+import { Nav, Navbar, Container, Button, Carousel } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <UncontrolledExample></UncontrolledExample>
+      <div className="main-project">
+        <ProjectList></ProjectList>
+      </div>
+      <Footer></Footer>
     </div>
   );
 }
 
+function UncontrolledExample() {
+  return (
+    <Carousel>
+    <Carousel.Item>
+        <div className='slidercontents'>
+            <div className='wrapText'>
+                <h1>Organic fresh fruits for your health</h1>
+                <div className="d-none d-md-block">
+                    <p>Interdum et malesuada fames ac ante ipsum primis in 
+                    faucibus. Mauris eleifend sagittis mollis. 
+                    Nulla finibus arcu eu tortor gravida aliquet</p>
+                </div>
+                <button>SHOP NOW</button>
+            </div>
+        </div>
+    </Carousel.Item>
+    {/* <Carousel.Item>
+        <div className='slidercontents2'>
+            <div className='wrapText'>
+                <h1>Organic fresh fruits for your health</h1>
+                <div className="d-none d-md-block">
+                    <p>Interdum et malesuada fames ac ante ipsum primis in 
+                    faucibus. Mauris eleifend sagittis mollis. 
+                    Nulla finibus arcu eu tortor gravida aliquet</p>
+                </div>
+                <button>SHOP NOW</button>
+            </div>
+        </div>
+    </Carousel.Item> */}
+</Carousel>
+  );
+}
+
+function ProjectList() {
+  return (
+    <>
+      <div className="container mt-3">
+        <div className="product">
+          <div className="thumbnail">
+            <div className="flex-grow-1 p-4">
+              <h5 className="title">아기다스 신발</h5>
+              <p className="date">2030년 1월 8일</p>
+              <p className="price">20000원</p>
+              <p className="floatEnd">?0</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+
+
+// db.collection('product').doc('상품3').set({제목 : '변기'} )
+
+// db.collection("product")
+//               .get()
+//               .then((snapshot) => {
+//                 snapshot.forEach((doc) => {
+
+//                 });
+//               });
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import firebase from "firebase/app";
+import "firebase/firestore";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +12,30 @@ root.render(
   </React.StrictMode>
 );
 
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAx-cRJFQgukcFWp2U0K1W4ixTjv46Q2OI",
+  authDomain: "gradproject-949d8.firebaseapp.com",
+  projectId: "gradproject-949d8",
+  storageBucket: "gradproject-949d8.appspot.com",
+  messagingSenderId: "154827914730",
+  appId: "1:154827914730:web:2059538495046b585e4693",
+  measurementId: "G-1C1X0N8JW2"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app); 혹시 나중에 오류 생기면 import 확인 !!
+
+export const db = firebase.firestore();
